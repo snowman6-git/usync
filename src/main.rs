22747900d2c914bb10b35db38c7e5cd16cb7efa7
snowman6-git <chldlfgks55@gmail.com>
2024-10-14 02:10:37 +0900

    modified:   .gitignore 	modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,7 @@ fn main() {
     if let Some(folder_path) = FileDialog::new().pick_folder() {
         let target = folder_path.display();
         println!("선택한 폴더: {}", target);
-
-
-
+        
         // 선택한 폴더 내의 파일 목록을 출력할 수 있음
         if let Ok(entries) = std::fs::read_dir(folder_path) {
             for entry in entries {
